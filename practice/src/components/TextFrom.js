0f371e7b0f5a6331d5ac9f129df29612ceecb730
Abhaya Nigam <abhaya.nigam13@gmail.com>
2023-08-06 19:51:33 +0530

    "Added More Logic"

--- practice/src/components/TextFrom.js
+++ practice/src/components/TextFrom.js
@@ -6,6 +6,11 @@ export default function TextForm(props) {
     setText(newText);
   };
 
+  const handleLowClick = () => {
+    let newText = text.toLowerCase();
+    setText(newText);
+  };
+
   const handleOnChange = (event) => {
     // console.log("On change");
     setText(event.target.value);
@@ -15,20 +20,40 @@ export default function TextForm(props) {
   // text = "new text"; // Wrong way to change the state
   // setText("new text"); // Correct way to change the state
   return (
-    <div>
-      <h1>{props.heading}</h1>
-      <div className="mb-3">
-        <textarea
-          className="form-control"
-          value={text}
-          onChange={handleOnChange}
-          id="myBox"
-          rows="8"
-        ></textarea>
+    <>
+      <div className="container">
+        <h1>{props.heading}</h1>
+        <div className="mb-3">
+          <textarea
+            className="form-control"
+            value={text}
+            onChange={handleOnChange}
+            id="myBox"
+            rows="8"
+          ></textarea>
+        </div>
+        <div class="d-grid gap-2">
+          <button class="btn btn-primary" type="button" onClick={handleUpClick}>
+            Convert to Uppercase
+          </button>
+          <button
+            class="btn btn-primary"
+            type="button"
+            onClick={handleLowClick}
+          >
+            Convert to Uppercase
+          </button>
+        </div>
       </div>
-      <button className="btn btn-primary" onClick={handleUpClick}>
-        Convert to Uppercase
-      </button>
-    </div>
+      <div className="container my-3">
+        <h1>Your Text Summary</h1>
+        <p>
+          {text.split(" ").length} words and {text.length} characters
+        </p>
+        <p>{0.008 * text.split(" ").length} Minutes to read</p>
+        <h2>Preview</h2>
+        <p>{text}</p>
+      </div>
+    </>
   );
 }
