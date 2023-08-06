8d5003170e97ec11175a08197693c559789a8bf9
Abhaya Nigam <abhaya.nigam13@gmail.com>
2023-08-06 10:47:54 +0530

    First React App

---
+++ practice/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+let name = "Abhaya";
+function App() {
+  return (
+    <>
+      <h1>Hello {name} !!</h1>
+      <h1>To your first React App 😁</h1>
+    </>
+  );
+}
+
+export default App;
