f602f84c61b77fc4e28b5797f4ebccfb3ad9683c
Abhaya Nigam <abhaya.nigam13@gmail.com>
2023-12-02 13:06:04 +0530

    Changes

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,22 +1,26 @@
-// import ReactDOM from "react-dom/client";
-// This version is used specifically for server-side rendering (SSR).
 import "./App.css";
 
-// This is a functional React component named App
+const element = (
+  <table>
+    <tr>
+      <th>Name</th>
+    </tr>
+    <tr>
+      <td>John</td>
+    </tr>
+    <tr>
+      <td>Elsa</td>
+    </tr>
+  </table>
+);
+
 function App() {
   return (
     <div className="App">
-      <h1> Hello World !! </h1>
+      <h1> Hello World ! </h1>
+      <p>{element}</p>
     </div>
   );
 }
 
-// Note : DOM (Document Object Model) is a platform and language-neutral interface
-//        that allows programs and scripts to dynamically access and update the content.
-
-// You can also run without these lines
-// const container = document.getElementById("root");
-// const root = ReactDOM.createRoot(container);
-// root.render(<App />); //renders the App component into the root created using createRoot
-
-export default App; // component, making it available for use in other parts of the application.
+export default App;
